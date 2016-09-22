c455d35f36547ebd83f48d688d61b9f07e6f01ca
Laurent Arnoud <laurent@spkdev.net>
2016-09-22 20:30:59 +0200

    Avoid leak of api key in integration tests

--- test/integration/api.rb
+++ test/integration/api.rb
@@ -2,22 +2,24 @@ require 'json'
 require 'money/bank/open_exchange_rates_bank'
 
 ERROR_MSG = 'Integration test failed!'.freeze
-
-puts 'OXR version', Money::Bank::OpenExchangeRatesBank::VERSION
-
 cache_path = '/tmp/latest.json'
 to_currency = 'CAD'
-oxr = Money::Bank::OpenExchangeRatesBank.new
-oxr.cache = cache_path
-oxr.app_id = ENV['OXR_APP_ID']
-oxr.update_rates
-
-Money.default_bank = oxr
-
-cad_rate = Money.default_bank.get_rate('USD', to_currency)
+app_id = ENV['OXR_APP_ID']
 
 begin
+  puts 'OXR version', Money::Bank::OpenExchangeRatesBank::VERSION
+
+  oxr = Money::Bank::OpenExchangeRatesBank.new
+  oxr.cache = cache_path
+  oxr.app_id = app_id
+  oxr.update_rates
+
+  Money.default_bank = oxr
+
   oxr.save_rates
+
+  cad_rate = Money.default_bank.get_rate('USD', to_currency)
+
   json_to_currency = JSON.parse(File.read(cache_path))['rates'][to_currency]
   puts 'JSON to_currency', json_to_currency
   puts 'Money to_currency', cad_rate
